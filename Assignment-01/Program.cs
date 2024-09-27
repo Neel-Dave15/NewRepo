5eb712e66a62b5b2d91c7b6974ce1e983d922400
Neel Jitendrakumar Dave <Ndave4906@conestogac.on.ca>
2024-09-27 16:06:22 -0400

    Committing the changes for assignment program for pet simulator.

---
+++ Assignment-01/Program.cs
@@ -0,0 +1,102 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool exitMain = false;
+        string petName = "";
+        string petType = "";
+
+        while (!exitMain)
+        {
+            Console.WriteLine("Welcome to Virtual Pet Simulator");
+            Console.WriteLine("Please choose a type of pet (Name or Number): ");
+            Console.WriteLine("1. Cat:");
+            Console.WriteLine("2. Dog:");
+            Console.WriteLine("3. Rabbit:");
+            Console.Write("User Input: ");
+            petType = Console.ReadLine();
+
+            switch (petType)
+            {
+                case "1":
+                    petType = "Cat";
+                    break;
+                case "2":
+                    petType = "Dog";
+                    break;
+                case "3":
+                    petType = "Rabbit";
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option choose again!");
+                    Console.WriteLine();
+                    continue; // Restart the loop for another attempt
+            }
+
+            Console.WriteLine("You have chosen a " + petType + " " + "What would you like to name your pet?");
+            Console.Write("Name: ");
+            petName = Console.ReadLine();
+            Console.WriteLine("Welcome " + petName + "! Let's take good care of him");
+            exitMain = true;
+        }
+
+        int hunger = 5;
+        int happiness = 5;
+        int health = 5;
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("\nMain Menu:");
+            Console.WriteLine("1. Feed Pet");
+            Console.WriteLine("2. Play with pet");
+            Console.WriteLine("3. Let Pet Rest");
+            Console.WriteLine("4. Check Pet Status");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    hunger = 3;
+                    happiness = 6;
+                    health = 7;
+                    break;
+                case "2":
+                    happiness = 9;
+                    hunger = 8;
+                    break;
+                case "3":
+                    health = 8;
+                    hunger = 5;
+                    happiness = 4;
+                    break;
+                case "4":
+                    Console.WriteLine();
+                    Console.WriteLine(petName + "'s Current Status: \n");
+                    Console.WriteLine("Happiness:" + happiness);
+                    Console.WriteLine("Hunger:" + hunger);
+                    Console.WriteLine("Health:" + health);
+                    continue; 
+                case "5":
+                    Console.WriteLine("Thank you for using the pet simulator!");
+                    exit = true;
+                    continue; // Exit the loop
+                default:
+                    Console.WriteLine("Invalid Choice Try Again");
+                    Console.Clear();
+                    continue; 
+            }
+
+            // Display the status after performing an action
+            Console.WriteLine( petName + "'s Status");
+            Console.Write("\n");
+            Console.WriteLine("Happiness:" + happiness );
+            Console.WriteLine("Hunger:" + hunger );
+            Console.WriteLine("Health:" + health);
+            Console.WriteLine();
+        }
+    }
+}
